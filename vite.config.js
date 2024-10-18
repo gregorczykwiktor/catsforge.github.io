fd79f668cab618099d3f152eb8a4a31370db3f90
gregorczykwiktor <48910956+gregorczykwiktor@users.noreply.github.com>
2024-10-18 22:07:01 +0200

    trying to public vite site

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  publicPath: process.env.NODE_ENV === "production" ? "/catsforge.github.io/" : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
