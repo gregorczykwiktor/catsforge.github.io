aed451f8aafa80f7e98f4adc1bdba307cef25b93
gregorczykwiktor <48910956+gregorczykwiktor@users.noreply.github.com>
2024-10-18 22:18:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
   ],
   publicPath: process.env.NODE_ENV === "production" ? "/catsforge.github.io/" : "/",
+  base: 'https://gregorczykwiktor.github.io/catsforge.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
