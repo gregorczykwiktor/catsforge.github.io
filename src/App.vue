afc1fbd02406976e4401d7833c1787f39ef7a44a
gregorczykwiktor <48910956+gregorczykwiktor@users.noreply.github.com>
2024-10-19 21:17:54 +0200

    good amount of good changes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import axios from 'axios';
+import { nextTick, ref } from 'vue';
+import cars from '../src/cars.json'
+
+const imageUrl = "https://s3-us-west-2.amazonaws.com/images.collecthw.com/"
+
+let fetchedCars = [{
+  AssortmentNum: "FPY86",
+  BaseCodes: "",
+  BaseColor: "Black",
+  BaseType: "Metal",
+  CardVariant: null,
+  CaseNum: "",
+  Col: "",
+  Color: "Green",
+  Country: "Thailand",
+  CurrentPrice: "0.00",
+  CurrentPriceDateTime: "2024-10-19 04:30:38",
+  DateAdded: "2024-07-12 17:12:21",
+  DateUpdated: "2024-07-12 17:12:21",
+  GalleryImage: "268ceefc-bcf2-4076-87c3-bad79c9dca80_{}.png",
+  Image: "8226_17207599023073356.png",
+  Images: "268ceefc-bcf2-4076-87c3-bad79c9dca80_{}.png",
+  InteriorColor: "Black",
+  Mainline: null,
+  ModelName: '"Rexy" Porsche 911 GT3 R (992)',
+  Notes: "",
+  OversizedCard: "1",
+  ReleaseAfter: null,
+  STH: null,
+  Scale: "1:64",
+  Series: "Car Culture - Race Day",
+  SeriesNum: "1",
+  TH: null,
+  Tampo: "Detailed taillights, AO Racing Rexy livery and graphics",
+  Toy: "HRV72",
+  ToyCard: "",
+  WheelType: "Black RR10SPM",
+  WindowColor: "Smoke Tint",
+  Year: "2024",
+  ebaySearchOverride: null,
+  id: "52672",
+  price: "0.00",
+  queueId: "110070",
+}];
+
+let fetchedCarsSorted = [];
+
+const itsFetching = ref(false);
+
+async function fetchData(){
+  await axios.get('/find?query=' + '2024' + '&length=9999')
+    .then((response) => {fetchedCars = response.data.data; console.log(fetchedCars)})
+    .catch((error) => {console.log(error)});
+    
+    fetchedCarsSorted = fetchedCars.sort(function(a, b){return a.toy - b.toy})
+    console.log(fetchedCarsSorted)
+
+  itsFetching.value = true;
+};
+
+function getImage(count){
+  let image = imageUrl + fetchedCars[count].Images.replace("{}", "large");
+  return image;
+}
+
+fetchData();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it! kutas" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    CatsForge
+    <table id="table">
+      <tbody v-if="itsFetching">
+      <tr v-for="item, count in fetchedCars">
+        <td>{{ count }}</td>
+        <td>{{ cars[count] }}</td>
+        <td>{{ fetchedCars[count].Toy }}</td>
+        <td>{{ fetchedCars[count].ModelName }}</td>
+        <!-- <td>{{imageUrl + fetchedCars[count].Images.replace("{}", "large")}}</td> -->
+        <td><img :src="getImage(count)"></td>
+      </tr>
+    </tbody>
+    </table>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.table{
+  border: 1px solid black;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      '/find?query=': {
+        target: 'https://collecthw.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [
     vue(),
   ],
